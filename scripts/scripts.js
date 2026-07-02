3755c51d12c332a9a5fc6ded6e130a8dc576b67f
Bhavani Seleri <Bhavani.Seleri@teknopoint.local>
2026-07-02 17:13:51 +0530

    added classname

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -126,24 +126,43 @@ export function decorateMain(main) {
  * Loads everything needed to get to LCP.
  * @param {Element} doc The container element
  */
-async function loadEager(doc) {
-  document.documentElement.lang = 'en';
-  decorateTemplateAndTheme();
-  const main = doc.querySelector('main');
-  if (main) {
-    decorateMain(main);
-    document.body.classList.add('appear');
-    await loadSection(main.querySelector('.section'), waitForFirstImage);
-  }
+async function loadLazy(doc) {
+  loadHeader(doc.querySelector('header'));
 
-  try {
-    /* if desktop (proxy for fast connection) or fonts already loaded, load fonts.css */
-    if (window.innerWidth >= 900 || sessionStorage.getItem('fonts-loaded')) {
-      loadFonts();
-    }
-  } catch (e) {
-    // do nothing
-  }
+  const main = doc.querySelector('main');
+  await loadSections(main);
+
+  const wrappers = main.querySelectorAll('.default-content-wrapper');
+
+  wrappers.forEach((wrapper) => {
+    wrapper.querySelectorAll(':scope > *').forEach((element) => {
+      switch (element.tagName.toLowerCase()) {
+        case 'h1':
+          element.classList.add('page-title');
+          break;
+
+        case 'ul':
+          element.classList.add('food-list');
+          break;
+
+        case 'p':
+          element.classList.add('image-wrapper');
+          break;
+
+        default:
+          break;
+      }
+    });
+  });
+
+  const { hash } = window.location;
+  const element = hash ? doc.getElementById(hash.substring(1)) : false;
+  if (hash && element) element.scrollIntoView();
+
+  loadFooter(doc.querySelector('footer'));
+
+  loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
+  loadFonts();
 }
 
 /**
